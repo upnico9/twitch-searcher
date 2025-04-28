023082cc6daaf790464f23dc1ccbc1d6406e9f81
upnico <nicolas.leroux.1@proton.me>
2025-04-28 18:40:30 +0200

    add a cors that enable everything to dev with no problems :)

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.search import router as search_router
 from app.api.autocomplete import router as autocomplete_router
@@ -8,6 +9,14 @@ app = FastAPI(
         description="Simple API to search Twitch videos by game name",
         version="0.1.0",
     )
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(search_router, prefix="/api")
 app.include_router(autocomplete_router, prefix="/api")
